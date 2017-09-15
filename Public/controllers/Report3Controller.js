6b0694a0dca61d5f053d2291f791621567d11472
awdashboard17 <awdashboard17@gmail.com>
2017-09-15 17:29:03 +0530

    update6

--- Public/controllers/Report3Controller.js
+++ Public/controllers/Report3Controller.js
@@ -71,28 +71,11 @@
       {
         console.log("*** IN getPageData ***");
         var rbnt = release + ":" + build + ":" + team ;
-        console.log("rbnt : " + rbnt);
-
-/*
-        var rnb = release + ":" + build;
-        $http.get('/getDistErrorstepsOfBuild/' + rnb).success( function ( response )
-        {
-            console.log("getDistErrorstepsOfBuild successful");
-            //console.log("response : " + response.values);
-            var errorstepsArray = [];
-            errorstepsArray = response.values;
-            //remove empty element
-            var index = errorstepsArray.indexOf('');
-            if (index > -1) {
-                errorstepsArray.splice(index, 1);
-            }
-            console.log("after : " + errorstepsArray);
-        });
-*/
-
+        //console.log("rbnt : " + rbnt);
 
         // This set is use to collect all the releases for selected rel, build and team. e.g. Tc1123, Tc1140 etc.
         var tcverSet = new Set();
+
         $http.get('/getResultsOfBuild/' + rbnt).success( function ( response )
         {
             //console.log("+++++++++++++++++++++ getResultsOfBuild successful");
@@ -133,18 +116,18 @@
                 // If team is same as the team selected by user.
                 if(team === resultDetails.Team)
                 {
-                    console.log("=========================================================================");
-                    console.log("build id : " + resultDetails.build_id);
-                    console.log("tc version : " + myTcversion);
-                    console.log("result : " + resultDetails.result);
+                    //console.log("=========================================================================");
+                    //console.log("build id : " + resultDetails.build_id);
+                    //console.log("tc version : " + myTcversion);
+                    //console.log("result : " + resultDetails.result);
                     //console.log("+++++++++++++++ team : " + team);
                     //console.log("+++++++++++++++ resultDetails.Team : " + resultDetails.Team);
 
                     // Form key object to store each row in report3
                     teamwiseKeyObj = '';
                     teamwiseKeyObj = release+"^^^"+build+"^^^"+team;
-                    console.log("--------------------------------------------------");
-                    console.log("teamwiseKeyObj : " + teamwiseKeyObj);
+                    //console.log("--------------------------------------------------");
+                    //console.log("teamwiseKeyObj : " + teamwiseKeyObj);
                     
                     tcverSet.add(myTcversion);
 
@@ -167,8 +150,8 @@
 
                     if(resultDetails.result==='failed')
                     {
-                        console.log("myTcArray is null : failed");
-                        console.log("incrementing count : myTcArray[" + myTcversion + "] = 1");
+                        //console.log("myTcArray is null : failed");
+                        //console.log("incrementing count : myTcArray[" + myTcversion + "] = 1");
                         var count = myTcArray[myTcversion];
                         myTcArray[myTcversion] = ++count;
                         myArray[errorstep] = myTcArray;
@@ -180,7 +163,7 @@
             $window.sessionStorage.setItem(rbnt+"Report3",JSON.stringify(myArray));
             var tcVersionArray = Array.from(tcverSet);
             tcVersionArray.sort();
-            console.log("after sorting tcVersionArray : " + tcVersionArray);
+            //console.log("after sorting tcVersionArray : " + tcVersionArray);
             $window.sessionStorage.setItem(rbnt+"tcSet"+"Report3",JSON.stringify(tcVersionArray));
             printReport3(rbnt);
                                 
@@ -221,7 +204,7 @@
         //Get the stored report3 item from sessionstorage
         var myArray = JSON.parse(sessionStorage.getItem(rbnt+"Report3"));
         var tcVersionArray = JSON.parse(sessionStorage.getItem(rbnt+"tcSet"+"Report3"));
-        console.log("tcVersionArray : " + tcVersionArray);
+        //console.log("tcVersionArray : " + tcVersionArray);
 
         //report3Array for storing row values. tcHeaderArray to store header values of report
         var report3Array = [];
@@ -230,7 +213,7 @@
 
         for(var key in myArray)
         {
-            console.log("*(myArray - key : " + key + " )*");
+            //console.log("*(myArray - key : " + key + " )*");
             var errorstep = key;
             //splash = key.split("^^^");
             //var application = splash[3];
@@ -242,10 +225,10 @@
             {
                 var tcKeysTempArray = Object.keys(myTcArray);
                 //console.log("tcKeysTempArray : " + tcKeysTempArray);
-                console.log("checking for findObjInArray(" + tcKeysTempArray + ", OBJECT - " + tcVersionArray[i] + ")");
+                //console.log("checking for findObjInArray(" + tcKeysTempArray + ", OBJECT - " + tcVersionArray[i] + ")");
                 if(!findObjInArray(tcKeysTempArray, tcVersionArray[i]))
                 {
-                    console.log("element not present. pushing myTcArray[" + tcVersionArray[i] + "] = 0");
+                    //console.log("element not present. pushing myTcArray[" + tcVersionArray[i] + "] = 0");
                     myTcArray[tcVersionArray[i]] = 0;
                 }
             }
@@ -256,30 +239,30 @@
                 var sorted_keys = Object.keys(myTcArray).sort();
                 for(i=0; i<sorted_keys.length; ++i)
                 {
-                    console.log(">>>>>>>>>>> : " + sorted_keys[i] + " : " + myTcArray[sorted_keys[i]]);
+                    //console.log(">>>>>>>>>>> : " + sorted_keys[i] + " : " + myTcArray[sorted_keys[i]]);
                     var value = myTcArray[sorted_keys[i]];
-                    console.log("myTcArray[" + sorted_keys[i] + "] = " + value);
+                    //console.log("myTcArray[" + sorted_keys[i] + "] = " + value);
                     tcValuesArray.push(value);
                 }
 
-                console.log(">>>>>>>>>>>> : before reverse tcValuesArray : " + tcValuesArray);
+                //console.log(">>>>>>>>>>>> : before reverse tcValuesArray : " + tcValuesArray);
                 tcValuesArray.reverse();
-                console.log(">>>>>>>>>>>> : after reverse tcValuesArray : " + tcValuesArray);
+                //console.log(">>>>>>>>>>>> : after reverse tcValuesArray : " + tcValuesArray);
 
                 //push values for tcresults only if it has failed values.
                 report3Array.push({feature: 'NA', errorstep: errorstep, tcvalues: tcValuesArray});
-                console.log("pushing : " + errorstep + ":" + tcValuesArray);
+                //console.log("pushing : " + errorstep + ":" + tcValuesArray);
             }
         }
         tcVersionArray.reverse();
         $scope.clients.tcHeaderArray2 = Array.from(tcVersionArray);
-        console.log("MYtcVersionArray : " + $scope.clients.tcHeaderArray2);
+        //console.log("MYtcVersionArray : " + $scope.clients.tcHeaderArray2);
         for(var i=0; i<tcVersionArray.length; ++i)
         {
             var splash = tcVersionArray[i].split('_');
             tcVersionArray[i] = splash[0];
         }
-        console.log("+++++++++++++ : after reverse tcHeaderArray : " + tcVersionArray);
+        //console.log("+++++++++++++ : after reverse tcHeaderArray : " + tcVersionArray);
         $scope.clients.tcHeaderArray = tcVersionArray;
         $scope.clients.report = report3Array;
     }
